90f43824c4fd703d83bbcb6dabc6e4819010c183
bhavyaCodes <xbhavyatomar@gmail.com>
2023-08-19 16:07:14 +0530

    add modal

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -1,5 +1,66 @@
 const Navbar = () => {
-  return <div className={`h-16`}>Navbar</div>;
+  return (
+    <div className="navbar bg-base-300 max-h-16 px-4">
+      <div className="flex-1">
+        {/* <a className="btn btn-ghost normal-case text-xl">daisyUI</a> */}
+        <div className="w-10 rounded-full">
+          <img src="/github-icon.svg" alt="logo" className="invert" />
+        </div>
+      </div>
+      <div className="flex-none gap-2">
+        <div className="form-control">
+          <input
+            value={""}
+            onClick={(e) => {
+              e.preventDefault();
+              // @ts-ignore
+              window.my_modal_2.showModal();
+            }}
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-24 md:w-auto"
+          />
+        </div>
+        <dialog id="my_modal_2" className="modal items-start">
+          <form method="dialog" className="modal-box mt-4">
+            <h3 className="font-bold text-lg">Hello!</h3>
+            <p className="py-4">Press ESC key or click outside to close</p>
+          </form>
+          <form method="dialog" className="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
+        {/* @ts-ignore */}
+        {/* <button onClick={() => window.my_modal_2.showModal()}>
+          view modal
+        </button> */}
+        {/* <div className="dropdown dropdown-end"> */}
+        {/* <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="/github-icon.svg" alt="logo" className="invert" />
+            </div>
+          </label> */}
+        {/* <ul
+            tabIndex={0}
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul> */}
+        {/* </div> */}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
