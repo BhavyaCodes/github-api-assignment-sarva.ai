d909b8f261ada101bb2757416f871f8f30861745
bhavyaCodes <xbhavyatomar@gmail.com>
2023-08-20 21:49:39 +0530

    style search bar on navbar

--- src/components/layout/Navbar/index.tsx
+++ src/components/layout/Navbar/index.tsx
@@ -44,19 +44,33 @@ const Navbar = () => {
       </div>
       <div className="flex-none gap-2">
         <div className="form-control">
-          <input
-            value={""}
+          <div
             onClick={(e) => {
               e.preventDefault();
               // @ts-ignore
               // window.search_modal.showModal();
               handleOpenModal();
             }}
-            readOnly
-            type="text"
-            placeholder="Search"
-            className="input input-bordered w-24 md:w-auto"
-          />
+            className="input input-bordered w-24 md:w-auto flex items-center"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              className="w-5 h-5"
+            >
+              <path
+                fillRule="evenodd"
+                d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
+                clipRule="evenodd"
+              />
+            </svg>
+
+            <p className="ml-2">Search </p>
+            <code className="text-xs ml-2 bg-base-300 p-1 rounded">
+              ctrl + k
+            </code>
+          </div>
         </div>
         <dialog id="search_modal" className="modal items-start">
           <form
