1276bf402e1ee6b42986aa7f55b64ad4943ecdfa
bhavyaCodes <xbhavyatomar@gmail.com>
2023-08-21 04:12:01 +0530

    signin working

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,9 +1,31 @@
 import { FC, ReactNode } from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 import Navbar from "./Navbar";
 
 const Layout: FC<{ children: ReactNode }> = ({ children }) => {
+  const { data: session, status } = useSession();
+
   return (
     <div>
+      {session ? (
+        <>
+          Signed in as {session.user?.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      ) : (
+        <>
+          Not signed in <br />
+          <a
+            href="/api/auth/signin"
+            onClick={(e) => {
+              e.preventDefault();
+              signIn();
+            }}
+          >
+            Sign in
+          </a>
+        </>
+      )}
       <Navbar />
       {children}
     </div>
